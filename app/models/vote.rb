de9b24be947effb6dad1ec789222de206ed824cb
Kevin Han <khan1782@gmail.com>
2017-02-02 15:31:10 -0600

    Remove typo on true

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,12 +3,12 @@ class Vote < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, { presence: true }
-  validates :value, numericality: { only_integer: :true }
-  validate :valid_vote_value
+  validates :value, numericality: { only_integer: true }
+  # validate :valid_vote_value
 
-  def valid_vote_value
-    unless self.value == -1 || self.value == 1
-      errors.add :value, "No voter fraud. Can only vote up or down. And only once."
-    end
-  end
+  # def valid_vote_value
+  #   unless self.value == -1 || self.value == 1
+  #     errors.add :value, "No voter fraud. Can only vote up or down. And only once."
+  #   end
+  # end
 end
