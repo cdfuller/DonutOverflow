f523774a52f8024f32b8bc0ebf5141d07a2d4e96
kevin <khan1782@gmail.com>
2017-02-03 14:37:31 -0600

    finished incorporating posting answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,12 +6,19 @@ post '/questions/:question_id/answers' do
   @answers = @question.answers
 
 	#create new answer from form info and current user login cookie
-	@current_answer = @question.answers.new(body: params[:body], user: current_user)
+	# @current_answer = @question.answers.new(body: params[:body], user: current_user)
+	@current_answer = Answer.new(body: params[:body], user: current_user, question_id: @question.id)
+
+
+
+	 
 	session[:user_id]=  nil
+	 
 
 	if @current_answer.save
 	 redirect "/questions/#{params[:question_id]}"
 	else
+
 		@errors = @current_answer.errors.full_messages
   	erb :'questions/show'
 	end
