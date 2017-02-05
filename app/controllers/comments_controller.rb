cf61992e9a7860215dd8adc562b6ba6d088155f1
Cody Fuller <mail@codyfuller.com>
2017-02-05 14:20:20 -0600

    Comment on questions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,3 +10,16 @@ post '/answers/:id' do
   end
 
 end
+
+
+post '/questions/:id/comments' do
+  @question = Question.find_by_id(params[:id])
+
+  @comment = Comment.new(body: params[:body], user: current_user, commentable: @question)
+  if @comment.save
+    redirect "/questions/#{@question.id}"
+  else
+    erb :'questions/show'
+  end
+
+end
